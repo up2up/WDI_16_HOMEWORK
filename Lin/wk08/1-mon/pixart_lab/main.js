ec4ee75238701d8907893e02e430fa3b5f4f4d13
Lin Yang <lin.yang@rea-group.com>
2018-08-06 17:06:29 +1000

    change color when user click each square

--- Lin/wk08/1-mon/pixart_lab/main.js
+++ Lin/wk08/1-mon/pixart_lab/main.js
@@ -4,10 +4,22 @@ const setColor = event => {
   $('.selected_color').css("background", form_data);
 }
 
+const enterKey = event => {
+  if (event.keyCode == 13) {
+    $(this).trigger("enterKey");
+  }
+}
 
+const changeColor = event => {
+  $(event.target).css('background', 'green');
+}
 
 
 $(document).ready(function () {
+  const allDivs = $('<div/>', {
+    'class': 'all_squares',
+  });
+
   for (var i = 1; i <= 1000; i++) {
     const divSquare = $('<div/>', {
       'class': 'square',
@@ -17,13 +29,12 @@ $(document).ready(function () {
         'border': '1px solid black'
       }
     });
-    divSquare.appendTo('body');
+    divSquare.appendTo(allDivs);
   }
+  allDivs.appendTo('body');
+
   $('#color_selector_form').submit(setColor);
   $('#color_selector_form').bind("enterKey", setColor);
-  $('#color_selector_form').keyup(function (e) {
-    if (e.keyCode == 13) {
-      $(this).trigger("enterKey");
-    }
-  });
+  $('#color_selector_form').keyup(enterKey);
+  $(allDivs).click(changeColor);
 });

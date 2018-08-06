5a0311b80a56b90f2c4e0e9c01cdda02df15792b
Lin Yang <lin.yang@rea-group.com>
2018-08-06 16:52:08 +1000

    create 1000 divs square

--- Lin/wk08/1-mon/pixart_lab/main.js
+++ Lin/wk08/1-mon/pixart_lab/main.js
@@ -8,6 +8,17 @@ const setColor = event => {
 
 
 $(document).ready(function () {
+  for (var i = 1; i <= 1000; i++) {
+    const divSquare = $('<div/>', {
+      'class': 'square',
+      css: {
+        'width': '20px',
+        'height': '20px',
+        'border': '1px solid black'
+      }
+    });
+    divSquare.appendTo('body');
+  }
   $('#color_selector_form').submit(setColor);
   $('#color_selector_form').bind("enterKey", setColor);
   $('#color_selector_form').keyup(function (e) {

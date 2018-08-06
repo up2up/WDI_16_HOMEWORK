b3e7fad3b8e2dc25f1b34e7d39da6f89b5596939
Lin Yang <lin.yang@rea-group.com>
2018-08-06 16:40:46 +1000

    enable enter key for select color from input

--- Lin/wk08/1-mon/pixart_lab/main.js
+++ Lin/wk08/1-mon/pixart_lab/main.js
@@ -1,10 +1,18 @@
 const setColor = event => {
   event.preventDefault();
   const form_data = $('form').serialize().replace("color=", "");
-  $('.selected_color').css("background",form_data);
+  $('.selected_color').css("background", form_data);
 }
 
 
+
+
 $(document).ready(function () {
-  $("#color_selector_form").submit(setColor);
+  $('#color_selector_form').submit(setColor);
+  $('#color_selector_form').bind("enterKey", setColor);
+  $('#color_selector_form').keyup(function (e) {
+    if (e.keyCode == 13) {
+      $(this).trigger("enterKey");
+    }
+  });
 });

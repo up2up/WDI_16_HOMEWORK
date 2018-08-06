577c963ecfa0f6fa6329508659dbfd804b623717
Lin Yang <lin.yang@rea-group.com>
2018-08-06 16:35:00 +1000

    apply user input color to brush box

---
+++ Lin/wk08/1-mon/pixart_lab/main.js
@@ -0,0 +1,10 @@
+const setColor = event => {
+  event.preventDefault();
+  const form_data = $('form').serialize().replace("color=", "");
+  $('.selected_color').css("background",form_data);
+}
+
+
+$(document).ready(function () {
+  $("#color_selector_form").submit(setColor);
+});
